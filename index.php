c25186523ccb4746ca89d4411942cc7441e16f1e
AnindaTantri <anindatantri25@gmail.com@mail.com>
2021-11-08 08:55:04 +0700

    update

--- index.php
+++ index.php
@@ -17,6 +17,7 @@ require_once "./tambah_config.php";
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <script src="https://kit.fontawesome.com/187119635b.js" crossorigin="anonymous"></script>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+        <link href="https://objectstorage.ap-sydney-1.oraclecloud.com/n/sdduwtrjkpfv/b/site/o/style.css" rel="stylesheet" type="text/css">
         <title>Daftar Diklat</title>
     </head>
     <body>
